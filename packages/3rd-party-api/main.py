875aa6f7f43e02cd2bc1f345c0c21d1e59a6c5f7
Julian Lima <julianlimalbs@gmail.com>
2024-03-12 11:25:55 -0300

    feat: implement 3rd party api route that returns json dummy data

--- packages/3rd-party-api/main.py
+++ packages/3rd-party-api/main.py
@@ -1,11 +1,22 @@
 """Module with the api routes"""
 
+import random
+from datetime import datetime
+import time
 from flask import Flask
 
 app = Flask(__name__)
 
+tickers = ["TICKER_1", "TICKER_2", "TICKER_3"]
 
-@app.route("/")
-def hello_world():
-    """Hello World!"""
-    return "<p>Hello, World!</p>"
+
+@app.route("/get_dummy_data", methods=["GET"])
+def get_dummy_data():
+    """Get dummy data"""
+    # Simulate a 1 second response time
+    time.sleep(1)
+    return {
+        "ticker": random.choice(tickers),
+        "price": random.randint(1, 1000),
+        "timestamp": datetime.now()
+    }
