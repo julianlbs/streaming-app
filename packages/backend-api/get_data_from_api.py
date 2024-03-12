0282b949f50734c30cb334d4e20af46598b0a34a
Julian Lima <julianlimalbs@gmail.com>
2024-03-12 14:20:57 -0300

    chore: update .pre-commit-config.yaml to include backend-api linting

--- packages/backend-api/get_data_from_api.py
+++ packages/backend-api/get_data_from_api.py
@@ -1,8 +1,15 @@
+"""
+Helper module to make requests to a 3rd party api
+"""
+
 import json
 import aiohttp
 
 
 async def get_data_from_api(url):
+    """
+    Get data from 3rd party api
+    """
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             data = await response.read()
